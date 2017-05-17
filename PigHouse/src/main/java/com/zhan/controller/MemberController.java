1291fba74f70fc70f34c546fb3553e7dd8c3e0d2
zhan <chenyunzhan08@126.com>
2017-05-17 17:18:01 +0800

    发送短信

--- PigHouse/src/main/java/com/zhan/controller/MemberController.java
+++ PigHouse/src/main/java/com/zhan/controller/MemberController.java
@@ -1,11 +1,21 @@
 package com.zhan.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.taobao.api.ApiException;
+import com.taobao.api.DefaultTaobaoClient;
+import com.taobao.api.TaobaoClient;
+import com.taobao.api.request.AlibabaAliqinFcSmsNumSendRequest;
+import com.taobao.api.response.AlibabaAliqinFcSmsNumSendResponse;
 
 @Controller
+@RequestMapping(path="/member")
 public class MemberController {
 	
     @RequestMapping("/greeting")
@@ -13,5 +23,30 @@ public class MemberController {
         model.addAttribute("name", name);
         return "greeting";
     }
+    
+    
+	@GetMapping(path="/getVerificationCode")
+	public @ResponseBody String getVerificationCode () {
+		String url = "http://gw.api.taobao.com/router/rest";
+		String appkey = "23828439";
+		String secret = "02f0e4b856e25fa3743f86f6daf47002";
+		TaobaoClient client = new DefaultTaobaoClient(url, appkey, secret);
+		AlibabaAliqinFcSmsNumSendRequest req = new AlibabaAliqinFcSmsNumSendRequest();
+		req.setExtend( "" );
+		req.setSmsType( "normal" );
+		req.setSmsFreeSignName( "陈云展测试" );
+		req.setSmsParamString( "{code:'58668'}" );
+		req.setRecNum( "18706734109" );
+		req.setSmsTemplateCode( "SMS_67300847" );
+		try {
+			AlibabaAliqinFcSmsNumSendResponse rsp = client.execute(req);
+			System.out.println(rsp.getBody());
+			return rsp.getBody();
+		} catch (ApiException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "{\"error\":\"获取验证友失败\"}";
+	}
 
 }
--- PigHouse/src/main/java/com/zhan/model/Member.java
+++ PigHouse/src/main/java/com/zhan/model/Member.java
@@ -1,5 +1,166 @@
 package com.zhan.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Member {
+	
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    
+    private String username;
+    private String password;
+    private String phone;
+    private String cardId;
+    private String cardImages;
+    private String balance;
+    private String deposit;
+    private String level;
+    private String email;
+    private String realName;
+    private String sex;
+	private String attribute0;
+	private String attribute1;
+	private String attribute2;
+	private String attribute3;
+	private String attribute4;
+	private String attribute5;
+	private String attribute6;
+	private String attribute7;
+	private String attribute9;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getCardId() {
+		return cardId;
+	}
+	public void setCardId(String cardId) {
+		this.cardId = cardId;
+	}
+	public String getCardImages() {
+		return cardImages;
+	}
+	public void setCardImages(String cardImages) {
+		this.cardImages = cardImages;
+	}
+	public String getBalance() {
+		return balance;
+	}
+	public void setBalance(String balance) {
+		this.balance = balance;
+	}
+	public String getDeposit() {
+		return deposit;
+	}
+	public void setDeposit(String deposit) {
+		this.deposit = deposit;
+	}
+	public String getLevel() {
+		return level;
+	}
+	public void setLevel(String level) {
+		this.level = level;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getRealName() {
+		return realName;
+	}
+	public void setRealName(String realName) {
+		this.realName = realName;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getAttribute0() {
+		return attribute0;
+	}
+	public void setAttribute0(String attribute0) {
+		this.attribute0 = attribute0;
+	}
+	public String getAttribute1() {
+		return attribute1;
+	}
+	public void setAttribute1(String attribute1) {
+		this.attribute1 = attribute1;
+	}
+	public String getAttribute2() {
+		return attribute2;
+	}
+	public void setAttribute2(String attribute2) {
+		this.attribute2 = attribute2;
+	}
+	public String getAttribute3() {
+		return attribute3;
+	}
+	public void setAttribute3(String attribute3) {
+		this.attribute3 = attribute3;
+	}
+	public String getAttribute4() {
+		return attribute4;
+	}
+	public void setAttribute4(String attribute4) {
+		this.attribute4 = attribute4;
+	}
+	public String getAttribute5() {
+		return attribute5;
+	}
+	public void setAttribute5(String attribute5) {
+		this.attribute5 = attribute5;
+	}
+	public String getAttribute6() {
+		return attribute6;
+	}
+	public void setAttribute6(String attribute6) {
+		this.attribute6 = attribute6;
+	}
+	public String getAttribute7() {
+		return attribute7;
+	}
+	public void setAttribute7(String attribute7) {
+		this.attribute7 = attribute7;
+	}
+	public String getAttribute9() {
+		return attribute9;
+	}
+	public void setAttribute9(String attribute9) {
+		this.attribute9 = attribute9;
+	}
+	
+	
+	
+	
 
 }
